1e1e696dcd8637bc4655210e68acd13391a6d8ae
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-06-10 12:10:08 +0000

    Added compton, termite, conky...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,9 @@
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+mkdir .config/termite/
+ln -s ~/.dotfiles/termite.conf ~/.config/termite/config
+ln -s ~/.dotfiles/compton.conf ~/.config/compton.conf
+mkdir .config/picom
+ln -s ~/.dotfiles/compton.conf ~/.config/picom/picom.conf
+mkdir ~/.config/conky
+ln -s ~/.dotfiles/conky ~/.config/conky/config
