6ebad98ba71fe8609cd36e42f0abbdaf467c57f5
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-06-10 12:10:17 +0000

    Improved layout handling

--- bspwm/scripts/layout.py
+++ bspwm/scripts/layout.py
@@ -6,19 +6,25 @@ path = f"{os.path.expanduser('~')}/.config/bspwm/settings.yaml"
 with open(path, "r") as settings:
     data = yaml.load(settings, Loader=yaml.Loader)
     layouts = data["layouts"]
+    layout_names = [layout["name"] for layout in data["layouts"]]
 current = [
     _
     for _ in check_output(["setxkbmap", "-query"]).decode("utf-8").split("\n")
     if "layout" in _
 ][0].replace("layout:     ", "")
 
-if current in layouts:
-    index = layouts.index(current) + 1
+if current in layout_names:
+    index = layout_names.index(current) + 1
 else:
     index = 0
 if index >= len(layouts):
     index = 0
 
-os.system(f"setxkbmap {layouts[index]}")
+os.system(f"setxkbmap {layouts[index]['name']}")
+for mod in layouts[index].get("mods", []):
+    if mod["type"] == "command":
+        os.system(mod["command"])
+    elif mod["type"] == "modmap":
+        os.system(f"xmodmap -e 'keycode {mod['keycode']} = {mod['chars']}'")
 
 del (index, layouts, current)
